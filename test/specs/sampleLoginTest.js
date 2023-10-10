c320da8fd630d15551ea3d11fa54b0797a94b746
Santhosh Kumar Sundaramurthy <santhoshkumar.sm@outlook.com>
2023-10-10 23:45:21 +0100

    Selectors learning

--- test/specs/sampleLoginTest.js
+++ test/specs/sampleLoginTest.js
@@ -1,7 +1,20 @@
 describe("Random Application", async () => {
-  it("Login page test", async () => {
+  it.skip("Login page test", async () => {
     await browser.url("https://www.google.com");
     let title = await browser.getTitle();
     console.log(title);
+    await expect(browser).toHaveTitle("Google");
+  });
+
+  it("Text field validation", async function () {
+    await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+    (await $("input[name='username']")).setValue("Hello World");
+    await browser.pause(5000);
+    (await $("#username")).setValue("rahulshettyacademy");
+    (await $("//input[@type='password']")).setValue("learning");
+    await browser.pause(5000);
+
+    (await $("#signInBtn")).click();
+    await browser.pause(5000);
   });
 });
