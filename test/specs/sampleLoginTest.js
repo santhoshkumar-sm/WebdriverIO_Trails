320b49c7eec831b2af51f901a50fa80cb320ec9d
Santhosh Kumar Sundaramurthy <santhoshkumar.sm@outlook.com>
2023-11-22 00:53:34 +0000

    Added configuration to run multiple browsers

--- test/specs/sampleLoginTest.js
+++ test/specs/sampleLoginTest.js
@@ -1,20 +1,34 @@
 describe("Random Application", async () => {
-  it.skip("Login page test", async () => {
+  xit("Login page test", async () => {
     await browser.url("https://www.google.com");
     let title = await browser.getTitle();
     console.log(title);
     await expect(browser).toHaveTitle("Google");
   });
 
-  it("Text field validation", async function () {
+  xit("Text field validation", async function () {
     await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
     (await $("input[name='username']")).setValue("Hello World");
-    await browser.pause(5000);
     (await $("#username")).setValue("rahulshettyacademy");
-    (await $("//input[@type='password']")).setValue("learning");
-    await browser.pause(5000);
+    (await $("//input[@type='password']")).setValue("learningg");
 
     (await $("#signInBtn")).click();
-    await browser.pause(5000);
+    let incorrectText = await $(".alert-danger").getText();
+    // console.log(incorrectText);
+    await browser.waitUntil(
+      async () => await $("#signInBtn").getAttribute("value"),
+      { timeout: 5000, timeoutMsg: "Element not found" }
+    );
+    // console.log(await $(".alert-danger").getText());
+  });
+
+  it("Successful login test", async () => {
+    await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+    await $("#username").setValue("rahulshettyacademy");
+    await $("//input[@type='password']").setValue("learning");
+    await $("#signInBtn").click();
+    await $(".btn-primary").waitForExist();
+    await expect(browser).toHaveUrlContaining("shop");
+    await expect(browser).toHaveTitle("ProtoCommerce");
   });
 });
